b638131a01ec9a0d166cd7710beb9c71493f7c0f
Peneaux Benjamin <91378569+VanHans@users.noreply.github.com>
2021-09-26 19:39:13 +0200

    Update AIStoryFR.bat

--- AIStoryFR.bat
+++ AIStoryFR.bat
@@ -5,8 +5,13 @@ set PYTHONIOENCODING=utf-8
 set PY="venv\python.exe"
 
 for %%X in (wt.exe) do (set FOUNDWT=%%~$PATH:X)
+
 if not defined FOUNDWT (
+
     start "AIStoryFR" %PY% main.py
+    
 ) else (
+
     wt.exe --title "AIStoryFR" -d "%~dp0/" -p "Windows PowerShell" %PY% main.py
+    
 )
